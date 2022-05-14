24c91b2a9aed53c2e6d5b761dfcb0c93b072c54f
paulinaMorenoA <105310671+paulinaMorenoA@users.noreply.github.com>
2022-05-13 20:25:51 -0500

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -310,7 +310,3 @@ variable "language_tag" {
   type        = string
   default     = "en-US"
 }
-variable "terraform_service_account" {
-  description = "Service account email of the account to impersonate to run Terraform."
-  type        = string
-}
